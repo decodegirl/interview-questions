ab61555cbe9d4e7fc198bc7b382ba92336d6c0cc
Celine Mwango <celinemwango2@gmail.com>
2019-06-27 13:42:20 -0600

    Add files via upload

---
+++ balance_parenthesis.py
@@ -0,0 +1,40 @@
+def balance_check(s):
+    # Check is even number of brackets
+    if len(s) % 2 != 0:
+        return False
+
+    # Set of opening brackets
+    opening = set('([{')
+
+    # Matching Pairs
+    matches = set([('(', ')'), ('[', ']'), ('{', '}')])
+
+    # Use a list as a "Stack"
+    stack = []
+
+    # Check every parenthesis in string
+    for paren in s:
+
+        # If its an opening, append it to list
+        if paren in opening:
+            stack.append(paren)
+
+        else:
+
+            # Check that there are parentheses in Stack
+            if len(stack) == 0:
+                return False
+
+            # Check the last open parenthesis
+            last_open = stack.pop()
+
+            # Check if it has a closing match
+            if (last_open, paren) not in matches:
+                return False
+
+    return len(stack) == 0
+
+
+
+print(balance_check('{['))
+print (balance_check('[]'))
